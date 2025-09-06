fe5403f7df7979df3ea417da402f9c0f44819fb1
Maksim Maksimchyk <maksim.maksimchyk@yandex.by>
2025-09-06 12:52:23 +0300

    Homework 4 done

--- src/Homework_4/Main.kt
+++ src/Homework_4/Main.kt
@@ -1,6 +1,3 @@
-import Homework_3.invertedLadder
-import Homework_3.primeNumber
-import Homework_3.singleDigit
 import kotlin.random.Random
 
 fun main() {
@@ -17,11 +14,10 @@ fun main() {
         inputMenu = readLine()?.toIntOrNull()
         when (inputMenu) {
             1 -> maxAndMinElement()
-            2 -> primeNumber()
-            3 -> singleDigit()
-            4 -> invertedLadder()
+            2 -> countOddAndEven()
+            3 -> reverseArray()
+            4 -> checkForPal()
             else -> break
-
         }
     } while (inputMenu != null)
 
@@ -29,10 +25,10 @@ fun main() {
 
 fun maxAndMinElement() {
 
-    val arr = Array(10) { Random.nextInt(1, 100) }
-    println("Сам массив с рандомыми числами: ")
+    val arr = Array(5) { Random.nextInt(1, 100) }
+    println("Сам массив из ${arr.size} элементов с рандомыми числами: ")
     arr.forEach {
-        println(it)
+        print("$it ")
     }
 
     var minNumber = arr[0]
@@ -51,8 +47,82 @@ fun maxAndMinElement() {
         }
     }
 
+    println()
     println("Минимальный элемент массива: $minNumber, с индексом $indexOfMinNumber")
     println("Максимальный элемент массива: $maxNumber, с индексом $indexOfMaxNumber")
 
 }
+
+fun countOddAndEven() {
+
+    val arr = Array(5) { Random.nextInt(1, 100) }
+    println("Сам массив из ${arr.size} элементов с рандомыми числами: ")
+    arr.forEach {
+        print("$it ")
+    }
+
+    var countEven = 0
+    var countOdd = 0
+
+    for (i in 0..arr.size - 1) {
+        if (arr[i] % 2 == 0) countEven++
+        else countOdd++
+    }
+
+    println()
+    println("Количество чётных: $countEven")
+    println("Количество нечётных: $countOdd")
+
+}
+
+fun reverseArray() {
+
+    val arr = Array(5) { Random.nextInt(1, 100) }
+    println("Изначальный массив из ${arr.size} элементов с рандомыми числами: ")
+    arr.forEach {
+        print("$it ")
+    }
+
+    val reversedArr = Array(arr.size) { 0 }
+    for (i in 0..arr.size - 1) {
+        reversedArr[i] = arr[arr.size - 1 - i]
+    }
+
+    println()
+    println("Перевернутый массив: ")
+    reversedArr.forEach {
+        print("$it ")
+    }
+}
+
+fun checkForPal() {
+
+    val arr = arrayOf(1, 1, 60, 61, 1)
+    println("Изначальный массив: ")
+    arr.forEach {
+        print("$it ")
+    }
+
+    val reversedArr = Array(arr.size) { 0 }
+    for (i in 0..arr.size - 1) {
+        reversedArr[i] = arr[arr.size - 1 - i]
+    }
+
+    println()
+    println("Перевернутый массив: ")
+    reversedArr.forEach {
+        print("$it ")
+    }
+
+    var isPal = true
+    for (i in 0..arr.size - 1) {
+        if (arr[i] != reversedArr[i]) isPal = false
+    }
+
+    println()
+    if (isPal == true) println("Массив является палиндромом")
+    else println("Массив не является палиндромом")
+
+}
+
 
