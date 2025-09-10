a6711e5ba13c34bb2af062b5a790e5bb4a05264e
Maksim Maksimchyk <maksim.maksimchyk@yandex.by>
2025-09-10 21:30:05 +0300

    Done

--- src/Homework_5/Homework_5.kt
+++ src/Homework_5/Homework_5.kt
@@ -15,15 +15,42 @@ fun main() {
         }
     } while (inputMenu != null)
 
-
 }
 
 
 fun reverseWordsInString() {
 
+    println("\nИзначальная строка: ")
+    val originalStr = "Hello World i am Kotlin"
+    println(originalStr)
+
+    val reversedStr = originalStr.split(' ').reversed().joinToString(" ")
+    println(reversedStr) //проверка
+
 }
 
 
 fun stringToCodes() {
+
+    println("\nИзначальная строка: ")
+    val originalStr = "Hello World i am Kotlin"
+    println(originalStr)
+
+    println("Преобразование строки в коды: ")
+    val strBuilderCodes = StringBuilder()
+    for (char in originalStr) {
+        strBuilderCodes.append(char.code)
+        strBuilderCodes.append(",")
+    }
+
+    val strOfCodes = strBuilderCodes.toString().dropLast(1) //чтобы удалить лишнюю запятую после последнего элемента
+    println(strOfCodes)
+
+    println("Теперь наоборот преобразование кодов в строку: ")
+    val strBuilderLetters = strOfCodes.split(",")
+    for (char in strBuilderLetters) {
+        print(char.toInt().toChar())
+    }
+    println()
 
 }
