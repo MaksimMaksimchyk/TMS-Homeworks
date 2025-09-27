0627d0e3fffb1b3bb766a2ea5fef798eb8dceddc
Maksim Maksimchyk <maksim.maksimchyk@yandex.by>
2025-09-27 12:17:41 +0300

    Homework 9 add special ability and distance

--- src/Homework_9/GameField.kt
+++ src/Homework_9/GameField.kt
@@ -7,71 +7,10 @@ object GameField {
     var distance = 10
     var turnCounter = 0
 
-    fun roll(): Int {
-        return Random.nextInt(1, 7)
-    }
-
-    fun nextTurn(firstPlayer: Player, secondPlayer: Player) {
-        turnCounter++
-        if (turnCounter == 1) {
-            println("Дуэль начинается...")
-            showStatus(firstPlayer, secondPlayer)
-        } else {
-            println("Ход №$turnCounter")
-            showStatus(firstPlayer, secondPlayer)
-        }
-
-        println("\n${firstPlayer.avatar} ${firstPlayer.name}, выберите действие: ")
-        println("❶ - Атаковать ${MyEmojies.ATTACK.emoji}")
-        println("❷ - Сдаться ${MyEmojies.SURRENDER.emoji}")
-        val inputMenu = readlnOrNull()?.toIntOrNull()
-        when (inputMenu) {
-            1 -> {
-                val rolled = roll()
-                println("${firstPlayer.name} успешно атакует")
-                secondPlayer.takeDamage(firstPlayer.attack(rolled))
-                if (secondPlayer.hitPoints <= 0) {
-                    congratWinner(firstPlayer)
-                    return
-                }
-            }
-
-            else -> {
-                firstPlayer.giveUp()
-                congratWinner(secondPlayer)
-                return
-            }
-        }
-
-
-        println("\n${secondPlayer.avatar} ${secondPlayer.name}, теперь ваша очередь: ")
-        println("❶ - Атаковать ${MyEmojies.ATTACK.emoji}")
-        println("❷ - Сдаться ${MyEmojies.SURRENDER.emoji}")
-        val inputMenu2 = readlnOrNull()?.toIntOrNull()
-        when (inputMenu2) {
-            1 -> {
-                val rolled = roll()
-                println("${secondPlayer.name} успешно атакует")
-                firstPlayer.takeDamage(secondPlayer.attack(rolled))
-                if (firstPlayer.hitPoints <= 0) {
-                    congratWinner(secondPlayer)
-                    return
-                }
-            }
-
-            else -> {
-                secondPlayer.giveUp()
-                congratWinner(firstPlayer)
-                return
-            }
-        }
-    }
-
 
     fun showStatus(firstPlayer: Player, secondPlayer: Player) {
-
         print(
-            "${firstPlayer.name} ${MyEmojies.HITPOINTS.emoji}${firstPlayer.hitPoints} " +
+            "\n${firstPlayer.name} ${MyEmojies.HITPOINTS.emoji}${firstPlayer.hitPoints} " +
                     "${MyEmojies.ARMORPOINTS.emoji}${firstPlayer.armorPoints}" +
                     " ${firstPlayer.avatar}"
         )
@@ -85,8 +24,116 @@ object GameField {
         )
     }
 
-    fun congratWinner(winner: Player) {
+
+    fun roll(): Int {
+        return Random.nextInt(1, 7)
+    }
+
+
+    fun showCube(rolled: Int, roller: Player) {
+        print("\n${roller.name} бросает кубик: ")
+        when (rolled) {
+            1 -> print(MyEmojies.CUBEONE.emoji)
+            2 -> print(MyEmojies.CUBETWO.emoji)
+            3 -> print(MyEmojies.CUBETHREE.emoji)
+            4 -> print(MyEmojies.CUBEFOUR.emoji)
+            5 -> print(MyEmojies.CUBEFIVE.emoji)
+            6 -> print(MyEmojies.CUBESIX.emoji)
+        }
+        print("\n")
+    }
+
+
+    fun congratsWinner(winner: Player) {
         println("${winner.name} победил! Ура!")
+    }
+
+
+    fun nextTurn(firstPlayer: Player, secondPlayer: Player) {
+        turnCounter++
+        println()
+        if (turnCounter == 1) {
+            println("Дуэль начинается...")
+            showStatus(firstPlayer, secondPlayer)
+        } else {
+            println("Ход №$turnCounter")
+            showStatus(firstPlayer, secondPlayer)
+        }
+
+        println("\n${firstPlayer.avatar} ${firstPlayer.name}, ваш ход.")
+        chooseAction(firstPlayer, secondPlayer)
+
+        showStatus(firstPlayer, secondPlayer)
+
+        if (firstPlayer.hitPoints > 0 && secondPlayer.hitPoints > 0) {
+            println("\n${secondPlayer.avatar} ${secondPlayer.name}, теперь ваша очередь. ")
+            chooseAction(secondPlayer, firstPlayer)
+        }
+
+    }
+
+
+    fun chooseAction(activePlayer: Player, passivePlayer: Player) {
+        var actionPoints = 3
+        while (actionPoints > 0) {
+            println("Осталось $actionPoints ${MyEmojies.ACTIONPOINTS.emoji}. Выберите действие:")
+            println("❶ - Атаковать ${MyEmojies.ATTACK.emoji} (2 ед. ${MyEmojies.ACTIONPOINTS.emoji})")
+            println("❷ - Подойти ближе ${MyEmojies.MOVEFORWARD.emoji} (1 ед.${MyEmojies.ACTIONPOINTS.emoji})")
+            println("❸ - Отойти подальше ${MyEmojies.MOVEBACK.emoji} (1 ед.${MyEmojies.ACTIONPOINTS.emoji})")
+            println("❹ - ${activePlayer.specialAbilityDescription} (3 ед.${MyEmojies.ACTIONPOINTS.emoji})")
+            println("Else - Сдаться ${MyEmojies.SURRENDER.emoji}")
+            val inputMenu = readlnOrNull()?.toIntOrNull()
+            when (inputMenu) {
+                1 -> {
+                    if (actionPoints >= 2) {
+                        actionPoints -= 2
+                        val rolled = roll()
+                        showCube(rolled, activePlayer)
+                        println("${activePlayer.name} успешно атакует")
+                        passivePlayer.takeDamage(activePlayer.calulateAttack(rolled))
+                        if (passivePlayer.hitPoints <= 0) {
+                            congratsWinner(activePlayer)
+                            return
+                        }
+                    } else {
+                        println("Недостаточно ${MyEmojies.ACTIONPOINTS.emoji}")
+                    }
+
+                }
+
+                2 -> {
+                    actionPoints -= 1
+                    activePlayer.moveForward()
+                }
+
+                3 -> {
+                    actionPoints -= 1
+                    activePlayer.moveBack()
+                }
+
+                4 -> {
+                    if (actionPoints == 3) {
+                        actionPoints -= 3
+                        activePlayer.useSpecialAbility(activePlayer, passivePlayer)
+                        if (passivePlayer.hitPoints <= 0) {
+                            congratsWinner(activePlayer)
+                            return
+                        }
+                    } else {
+                        println("Недостаточно ${MyEmojies.ACTIONPOINTS.emoji}")
+                    }
+
+                }
+
+                else -> {
+                    activePlayer.giveUp()
+                    congratsWinner(passivePlayer)
+                    return
+                }
+            }
+        }
+
+
     }
 
 
--- src/Homework_9/Player.kt
+++ src/Homework_9/Player.kt
@@ -1,5 +1,7 @@
 package Homework_9
 
+import kotlin.random.Random
+
 class Player() {
     var name = "New Player"
     var attackPoints = 0
@@ -7,15 +9,16 @@ class Player() {
     var hitPoints = 0
     var damageReducePoints = 0
     var avatar = ""
+    var specialAbilityDescription = ""
     var gamingClas = GamingClasses.NOOB
 
     fun chooseGamingClas() {
         println("${this.name}, выберите класс:")
         var inputMenu: Int? = null
         do {
-            println("❶ - Паладин ${MyEmojies.PALADIN.emoji}. Много здоровья, слабая атака, тяжелая броня. Силён в ближнем бою.")
-            println("❷ - Лучник ${MyEmojies.ARCHER.emoji}. Очень сильная атака, мало здоровья, легкая броня. Силён в дальнем бою.")
-            println("❸ - Разбойник ${MyEmojies.ROGUE.emoji}. Сильная атака, среднее количество здоровья, средняя броня. Универсальный боец.")
+            println("❶ - Паладин ${MyEmojies.PALADIN.emoji}. Много здоровья, слабая атака, тяжелая броня. Силён в ближнем бою. Может восстанавливать здоровье.")
+            println("❷ - Лучник ${MyEmojies.ARCHER.emoji}. Очень сильная атака, мало здоровья, легкая броня. Силён в дальнем бою. Может убить с одного выстрела.")
+            println("❸ - Разбойник ${MyEmojies.ROGUE.emoji}. Сильная атака, среднее количество здоровья, средняя броня. Универсальный боец. Может красть здоровье у противника")
             inputMenu = readlnOrNull()?.toIntOrNull()
             when (inputMenu) {
                 1 -> this.setAttributes(chosenGamingClas = GamingClasses.PALADIN)
@@ -34,30 +37,33 @@ class Player() {
             GamingClasses.PALADIN -> {
                 this.attackPoints = 1
                 this.armorPoints = 10
-                this.hitPoints = 30
+                this.hitPoints = 60
                 this.damageReducePoints = 3
                 this.avatar = MyEmojies.PALADIN.emoji
                 this.gamingClas = GamingClasses.PALADIN
+                this.specialAbilityDescription = "Восстановить 15 ед. здоровья"
                 println("Отличный выбор, ${this.name}! Теперь Вы Паладин!")
             }
 
             GamingClasses.ARCHER -> {
                 this.attackPoints = 3
                 this.armorPoints = 3
-                this.hitPoints = 12
+                this.hitPoints = 15
                 this.damageReducePoints = 1
                 this.avatar = MyEmojies.ARCHER.emoji
                 this.gamingClas = GamingClasses.ARCHER
+                this.specialAbilityDescription = "Выстрелить, целясь в голову. С вероятностью 10% наносит смертельный урон. "
                 println("Отличный выбор, ${this.name}! Теперь Вы Лучник!")
             }
 
             GamingClasses.ROGUE -> {
                 this.attackPoints = 2
                 this.armorPoints = 6
-                this.hitPoints = 21
+                this.hitPoints = 25
                 this.damageReducePoints = 2
                 this.avatar = MyEmojies.ROGUE.emoji
                 this.gamingClas = GamingClasses.ROGUE
+                this.specialAbilityDescription = "Украсть 5 ед. здоровья у противника"
                 println("Отличный выбор, ${this.name}! Теперь Вы Разбойник!")
             }
 
@@ -68,15 +74,35 @@ class Player() {
     }
 
 
-    fun attack(rolled: Int): Int {
-        return rolled * this.attackPoints
+    fun calulateAttack(rolled: Int): Int {
+        var distanceFactor = 0
+        if (this.gamingClas == GamingClasses.PALADIN) {
+            when (GameField.distance) {
+                10 -> 0
+                in 4..9 -> distanceFactor = 4
+                in 0..3 -> distanceFactor = 6
+                in 11..99 -> distanceFactor = -1
+            }
+        }
+
+        if (this.gamingClas == GamingClasses.ARCHER) {
+            when (GameField.distance) {
+                10 -> 0
+                in 4..9 -> distanceFactor = -1
+                in 0..3 -> distanceFactor = -1
+                in 11..19 -> distanceFactor = 4
+                in 20..99 -> distanceFactor = 6
+            }
+        }
+
+        return rolled * this.attackPoints + distanceFactor
     }
 
     fun takeDamage(damage: Int) {
         if (this.armorPoints > 0) {
             this.armorPoints -= 1
             println("${this.name} теряет 1 ед. брони${MyEmojies.ARMORPOINTS.emoji}")
-        } else{
+        } else {
             this.damageReducePoints = 0
             println("${this.name} теряет свою броню${MyEmojies.ARMORPOINTS.emoji}")
         }
@@ -88,15 +114,51 @@ class Player() {
     }
 
     fun moveBack() {
-        GameField.distance++
+        println("${this.name}${this.avatar} убегает")
+        GameField.distance += 2
     }
 
+
     fun moveForward() {
-        GameField.distance--
+        if (GameField.distance > 1) {
+            println("${this.name}${this.avatar} подходит ближе")
+            GameField.distance -= 2
+        } else {
+            println("${this.name}${this.avatar} Попытался подойти, но вы и так стоите вплотную")
+        }
+
     }
 
     fun giveUp() {
         this.hitPoints = 0
+    }
+
+
+    fun useSpecialAbility(activePlayer: Player, passivePlayer: Player) {
+        when (this.gamingClas) {
+            GamingClasses.PALADIN -> {
+                println("${activePlayer.avatar}${activePlayer.name} восстанавливает 15 ${MyEmojies.HITPOINTS.emoji}")
+                activePlayer.hitPoints += 15
+            }
+
+            GamingClasses.ARCHER -> {
+                val roll = Random.nextInt(1, 11)
+                if (roll == 1) {
+                    println("${activePlayer.avatar}${activePlayer.name} Попадает в голову!")
+                    passivePlayer.hitPoints = 0
+                } else {
+                    println("${activePlayer.avatar}${activePlayer.name} промахивается")
+                }
+            }
+            GamingClasses.ROGUE -> {
+                println("${activePlayer.avatar}${activePlayer.name} восстанавливает 5 ${MyEmojies.HITPOINTS.emoji}, а ${passivePlayer.avatar}${passivePlayer.name} теряет 5 ${MyEmojies.HITPOINTS.emoji}")
+                activePlayer.hitPoints += 5
+                passivePlayer.hitPoints -= 5
+            }
+            else -> {
+                println("Вы ещё нуб")
+            }
+        }
     }
 
 
